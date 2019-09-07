2bd5eeacedd12ae73aff0d1db78f39c2c186ea4c
jaredpooole <jaredpoole123@gmail.com>
2019-09-07 07:49:38 -0400

    Added pin definitions

--- switch-firmware/switch-firmware.ino
+++ switch-firmware/switch-firmware.ino
@@ -1,4 +1,18 @@
 
+#define SERIAL_ENABLE PD4
+#define BUTTON PD3 //Also INT1
+#define SERVO PB1
+#define BUZZER PB5
+#define TRANSMITTER PD5
+#define RECEIVER PD6
+#define VAR_RESISTOR PC0
+
+
+#define BMS_INTERRUPT PD2 //Also INT0
+#define BMS_OTG PC3
+#define BMS_CE PC2
+#define BMS_QON PC1
+
 
 void setup() {
   // put your setup code here, to run once:
